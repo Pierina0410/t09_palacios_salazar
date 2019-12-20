1ea3f5a20bf20ff4df21e8070984f1f794b1bcf3
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-20 12:51:42 -0500

    app8

---
+++ palacios/app8.py
@@ -0,0 +1,8 @@
+#programa para colocar el número de desbloqueo de celular
+import libreria
+import os
+x=str(os.sys.argv[1])
+m=libreria.desbloqueo(x)
+msg="EL NÚMERO DE DESBKLOQUEO DE CELULAR ES:{}"
+print(msg.format(x,m))
+
