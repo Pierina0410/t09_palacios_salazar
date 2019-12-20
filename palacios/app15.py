e6f49121c6335182f764bd54b0130ac3b7e532ca
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-20 12:55:47 -0500

    app15

---
+++ palacios/app15.py
@@ -0,0 +1,10 @@
+#calcular el numero de la tinka
+import libreria
+import os
+
+a=int(os.sys.argv[1])
+
+
+m=libreria.num_tinka(a,b)
+msg="El numero de tinka de  {} es: {}"
+print(msg.format(a,b,m))
