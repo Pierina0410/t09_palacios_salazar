d4005cec0d2850853b163bee6a780da9430e8db6
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-20 12:56:06 -0500

    app16

---
+++ palacios/app16.py
@@ -0,0 +1,10 @@
+#calcular el numero de la temperatura
+import libreria
+import os
+
+a=int(os.sys.argv[1])
+
+
+m=libreria.temperatura(a,b)
+msg="El numero de temperatura de  {} es: {}"
+print(msg.format(a,m))
